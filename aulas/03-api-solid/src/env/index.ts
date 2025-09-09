d7fb2ebd809c6061b6810c63eed9afc062cad2ec
duizz <luizcod025@gmail.com>
2025-09-09 20:54:05 -0300

    initial commit

---
+++ aulas/03-api-solid/src/env/index.ts
@@ -0,0 +1,16 @@
+import { coerce, z } from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: coerce.number()
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false){
+    console.error("Invalid environment variables", _env.error.format())
+
+    throw new Error('Invalid environment vatiables.')
+}
+
+export const env = _env.data
---
+++ aulas/03-api-solid/src/server.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+import { env } from "./env/index";
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT
+})
+.then(() => console.log("Server HTTP running!"))
