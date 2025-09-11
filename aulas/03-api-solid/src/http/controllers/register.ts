2ad9d06234a76051bc890f87fefb0b1332adfe6d
duizz <luizcod025@gmail.com>
2025-09-10 21:40:50 -0300

    add docker config, add database connection and implemented register user route

---
+++ aulas/03-api-solid/src/http/controllers/register.ts
@@ -0,0 +1,27 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from 'zod'
+import { registerUseCase } from "@/use-cases/register";
+
+export async function register (request: FastifyRequest, reply: FastifyReply) {
+
+    const registerSchema = z.object({
+        name: z.string(),
+        email: z.email(),
+        password: z.string().min(6)
+    })
+
+    const { name, email, password } = registerSchema.parse(request.body)
+    
+    try{
+        await registerUseCase({
+            name,
+            email,
+            password
+        })
+    } catch(err) {
+        return reply.status(409).send()
+    }
+
+    return reply.status(201).send()
+            
+}
